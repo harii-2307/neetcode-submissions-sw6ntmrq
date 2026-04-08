87fd126c51ac00588014b4b763b136427c4ce96e
Kuruba Haritha <harizz2307@gmail.com>
2026-04-09 01:45:41 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans;
+        int n=nums.size();
+        int sum=(n*(n+1))/2;
+        int temp=0;
+        for(int i=0;i<n;i++){
+            temp+=nums[i];
+
+        }
+        ans=sum-temp;
+        return ans;
+        
+    }
+};
